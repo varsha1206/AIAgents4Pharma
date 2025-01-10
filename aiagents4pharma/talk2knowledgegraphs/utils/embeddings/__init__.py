e5498abeae42e64b24865f44fdd0f70746ac34c6
AW Mulyadi <awmulyadi@gmail.com>
2025-01-10 09:47:10 +0100

    feat: initial setup for textual embeddings

---
+++ aiagents4pharma/talk2knowledgegraphs/utils/embeddings/__init__.py
@@ -0,0 +1,7 @@
+'''
+This file is used to import all the models in the package.
+'''
+from . import embeddings
+from . import embedding_with_ollama
+from . import embedding_with_huggingface
+from . import embedding_with_sentence_transformer
