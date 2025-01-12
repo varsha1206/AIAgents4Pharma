4a3adb95653ad8b64ebd8f5d4e7e066cf799edf6
awmulyadi <awmulyadi@gmail.com>
2025-01-12 20:16:43 +0100

    feat: update textual embeddings for starkqa-primekg

--- aiagents4pharma/talk2knowledgegraphs/utils/embeddings/__init__.py
+++ aiagents4pharma/talk2knowledgegraphs/utils/embeddings/__init__.py
@@ -2,6 +2,4 @@
 This file is used to import all the models in the package.
 '''
 from . import embeddings
-from . import embedding_with_ollama
-from . import embedding_with_huggingface
 from . import embedding_with_sentence_transformer
