034544ebd0c833d944dffea1af0fd741d1e4255b
varsha1206 <teamleoeaton@gmail.com>
2025-05-23 18:05:29 +0200

    Removing PMC and PM ids from being displayed in dataframe

--- app/frontend/utils/streamlit_utils.py
+++ app/frontend/utils/streamlit_utils.py
@@ -736,6 +736,8 @@ def get_response(agent, graphs_visuals, app, st, prompt):
                 "Abstract",
                 "Key",
                 "arxiv_id",
+                "pmc_id",
+                "pm_id",
                 "semantic_scholar_paper_id",
                 "source",
                 "filename",
